4c9b3ec22b171f15ba2d6e930a6a2f48f62badbd
Camsical <mr.camsical.cowie@gmail.com>
2023-11-14 20:32:36 +1030

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+var height = window.innerHeight - 70
+
+const crocsType = document.getElementById('crocs_type')
+const jibbitzType = document.getElementById('jibbitz_type')
+const titleBar = document.getElementById('titleBar')
+const title = document.getElementById('title')
+
+
+titleBar.style.width = window.innerWidth + 'px'
+title.style.left = window.innerWidth/2 - 250 + 'px'
+crocsType.style.height = height+'px'
+jibbitzType.style.height = height+'px'
+
